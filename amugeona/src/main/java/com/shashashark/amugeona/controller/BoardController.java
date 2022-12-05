d7dbd4c4d343c3e316d6220dfab880e8682e80c4
sharksumin <1379psm@naver.com>
2022-12-05 22:28:37 +0900

    코드리뷰

--- amugeona/src/main/java/com/shashashark/amugeona/controller/BoardController.java
+++ amugeona/src/main/java/com/shashashark/amugeona/controller/BoardController.java
@@ -5,10 +5,7 @@ import com.shashashark.amugeona.model.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,8 @@ public class BoardController {
         return new ResponseEntity<>(boardService.selectOne(boardSeq).orElseThrow(), HttpStatus.OK);
     }
 
-
+    @DeleteMapping("/delete")
+    public  ResponseEntity<?> deleteBoard(){
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
