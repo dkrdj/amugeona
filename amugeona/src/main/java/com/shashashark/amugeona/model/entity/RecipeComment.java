824b33a516114dd7a753e9eb3300d1ebbb98b090
dkrdj <dkrdj1000@naver.com>
2023-01-15 16:51:54 +0900

    테이블명 수정

--- amugeona/src/main/java/com/shashashark/amugeona/model/entity/RecipeComment.java
+++ amugeona/src/main/java/com/shashashark/amugeona/model/entity/RecipeComment.java
@@ -12,7 +12,6 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Table(name = "recipes_comment")
 public class RecipeComment extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
